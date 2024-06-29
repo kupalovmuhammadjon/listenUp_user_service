013784f41f6c9339dee73a8c402302e454f6beea
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-06-29 16:45:35 +0500

    service user update and delete methods added

--- service/user.go
+++ service/user.go
@@ -1,15 +1,54 @@
 package service
 
 import (
-	"user_service/storage/postgres"
+  "context"
+  "database/sql"
+  "fmt"
+  pb "user_service/genproto/user"
+  "user_service/storage/postgres"
 )
 
-type Users struct {
-	Users *postgres.UserRepo
+type UserManagement struct {
+  Users *postgres.UserRepo
 }
 
-func NewUsers(users *postgres.UserRepo) *Users {
-	return &Users{Users: users}
+func NewUserManagement(db *sql.DB) *UserManagement {
+  users := postgres.NewUserRepo(db)
+  return &UserManagement{Users: users}
 }
 
-// func (u *Users) GetUserById(ctx *context.Context, req.)
+func (u *UserManagement) GetUserById(ctx *context.Context, req *pb.ID) (*pb.User, error) {
+  if len(req.Id) != 32 {
+    return nil, fmt.Errorf("id is not valid")
+  }
+  user, err := u.Users.GetUserById(req.Id)
+  if err != nil {
+    return nil, err
+  }
+  return &pb.User{
+    Id:        user.Id,
+    Username:  user.Username,
+    Email:     user.Email,
+    Password:  user.Password,
+    CreatedAt: user.CreatedAt,
+    UpdatedAt: user.UpdatedAt,
+  }, nil
+}
+
+func (u *UserManagement) UpdateUser(ctx context.Context, req *pb.User) (*pb.Status, error) {
+	err := u.Users.UpdateUser(req)
+	if err != nil {
+		return &pb.Status{Status: "failed"}, err
+	}
+
+	return &pb.Status{Status: "success"}, nil
+}
+
+func (u *UserManagement) DeleteUser(ctx context.Context, req *pb.ID) (*pb.Status, error) {
+	err := u.Users.DeleteUser(req.Id)
+	if err != nil {
+		return &pb.Status{Status: "failed"}, err
+	}
+
+	return &pb.Status{Status: "success"}, nil
+}
