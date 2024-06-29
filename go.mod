b90165afd4bb5bf8938aad4248e869c473f99bd4
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-06-29 15:46:56 +0500

    put method added

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.4
 
 require (
 	github.com/joho/godotenv v1.5.1
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/cast v1.6.0
 	google.golang.org/grpc v1.64.0
 	google.golang.org/protobuf v1.34.2
--- storage/postgres/user.go
+++ storage/postgres/user.go
@@ -1,1 +1,44 @@
 package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+	"user_service/genproto/user_management"
+
+	"github.com/pkg/errors"
+)
+
+type UserRepo struct {
+	DB *sql.DB
+}
+
+func NewUserRepo(db *sql.DB) *UserRepo {
+	return &UserRepo{DB: db}
+}
+
+func (u *UserRepo) Put(user *user_management.User) error {
+	query := "update users set "
+	params := []interface{}{}
+
+	if user.Username != "" {
+		query += fmt.Sprintf("username = $%d, ", len(params)+1)
+		params = append(params, user.Username)
+	}
+	if user.Email != "" {
+		query += fmt.Sprintf("email = $%d, ", len(params)+1)
+		params = append(params, user.Email)
+	}
+	if user.Password != "" {
+		query += fmt.Sprintf("password_hash = $%d, ", len(params)+1)
+	}
+	query += fmt.Sprintf("updated_at = $%d, ", len(params)+1)
+	params = append(params, time.Now())
+
+	_, err := u.DB.Exec(query, params...)
+	if err != nil {
+		return errors.Wrap(err, "failed to update user")
+	}
+	
+	return nil
+}
